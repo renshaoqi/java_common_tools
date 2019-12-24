747e34a87fb52114772b10f21820fb2bdb6b31a9
renshaoqi <2303409366@qq.com>
2019-12-24 14:37:23 +0800

    数组练习代码更新

--- src/main/java/com/zeroten/common/util/CheckUtils.java
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -8,7 +8,7 @@ public class CheckUtils {
         // String... strings 为可变参数，你可以认为它是 String[] Strings 。 如果 strings 中有任意一个字符串为空，则返回false
         // 否则返回 true
         for (int index = 0; index < strings.length; index++) {
-            if (strings[index] == " ") {
+            if (strings[index].length() == 0) {
                 return false;
             }
         }
@@ -17,34 +17,39 @@ public class CheckUtils {
 
     public static boolean isEmpty(Object[] arr) {
         // 判断引用类型数组是否为空，为空或 null 则返回 true，否则返回 false
-        if (arr == null || arr.length == 0) {
-            return true;
-        }
-        return false;
+//        if (arr == null || arr.length == 0) {
+//            return true;
+//        }
+//        return false;
+        return arr.length == 0 || arr == null;
     }
+
 
     public static boolean equals(String str1, String str2) {
         // 判断 str1 字符串是否相等，相等则返回 true，否则返回 false。当其中一个是 null 时 返回false
-        if (str1 == null || str2 == null) {
-            return false;
-        }
+//        if (str1 == null || str2 == null) {
+//            return false;
+//        }
+//
+//        if (str1.equals(str2)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
 
-        if (str1.equals(str2)) {
-            return true;
-        } else {
-            return false;
-        }
+        return str1 != null && str1.equals(str2);
     }
 
     public static boolean equals(Integer n1, Integer n2) {
         // 判断 n1 和 n2 的值是否相等，相等则返回 true，否则返回 false。当其中一个是 null 时返回 false。
-        if (n1 == null || n2 == null) {
-            return false;
-        }
-        if (n1 == n2) {
-            return true;
-        } else {
-            return false;
-        }
+//        if (n1 == null || n2 == null) {
+//            return false;
+//        }
+//        if (n1.equals(n2)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+        return n1 != null && n1.equals(n2);
     }
 }
